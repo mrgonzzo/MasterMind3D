edc897d590fdae72e05538077ed13a9611225fd4
mrgonzzo <mr.gonzzo@gmail.com>
2025-06-09 15:07:00 +0200

    añadido boton nueva partida

--- Assets/Scripts/closeApp.cs
+++ Assets/Scripts/closeApp.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class ExitGame : MonoBehaviour
+public class AppController : MonoBehaviour
 {
     public void Salir()
     {
         Application.Quit();
         Debug.Log("La aplicación se está cerrando...");
+    }
+
+    public void NuevaPartida()
+    {
+        // Recarga la escena actual
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
